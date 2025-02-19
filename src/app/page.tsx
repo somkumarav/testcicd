faa9a864f69d6a448ffb7dfe0a193db3d6a3d112
somkumarav <somkumarav@gmail.com>
2025-02-19 15:25:37 +0530

    final touches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,21 +95,6 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/globe.svg'
-            alt='Globe icon'
-            width={16}
-            height={16}
-          />
-          hello there
-        </a>
       </footer>
     </div>
   );
